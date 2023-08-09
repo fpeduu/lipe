b1f4376d1a5cf020e20f0eebf8a730c5103d5097
lipe <fefs@cin.ufpe.br>
2023-08-09 00:45:12 -0300

    FEAT: about me page

--- src/pages/sobre-mim/index.jsx
+++ src/pages/sobre-mim/index.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import "./style.css";
 
 function Aboutme() {
-  return <div>Aboutme</div>;
+  return (
+    <div className="d-flex flex-column align-items-center w-100 p-5 about-me-container">
+      <h1>Quem é lipe?</h1>
+
+      <div className="text-sheet mt-4">
+        <p>
+          Filipe Eduardo Ferreira da Silva (aka <strong>lipe</strong>) tem 19
+          anos e adora explorar <span>c</span>oisas novas. A busca de coisas que
+          prendam seu interesse é, desde pequeno e ainda <span>ho</span>je, seu
+          passatempo favorito. Desde advogado até psi<span>c</span>ólogo, lipe
+          já cogitou várias opções de carreira, até que decidiu (em agosto de
+          2019) que queria passar, ao menos, os próximos anos da sua vida na
+          frente de um computador, escrevendo programas. Depois de um tempo,
+          aprender deixou de ser uma atividade e se tornou uma{" "}
+          <strong>paixão</strong>.
+        </p>
+
+        <p>
+          Lipe, apesar de meio disperso, gosta de estar disponível às pessoas
+          que ama. Ele acredita que uma boa relação com amigos e família é{" "}
+          <strong>essencial</strong> na vida. As coisas que mais gosta de fazer
+          são jogar, passar um bom tempo com amigos e s<span>ol</span>ucionar
+          problemas que chamam sua atenção. Desde o ensino médio, ele percebeu
+          que gostav<span>a</span> de se aprofundar nas áreas de seu in
+          <span>te</span>resse, e de trabalhar em conjunto com outras pessoas.
+          Por fim, depois de experimentar diversas áreas e pensar em diversos
+          cursos diferentes, ele decidiu virar programador :').
+        </p>
+
+        <p>
+          Por último, mas não menos importante, algumas de suas coisas favoritas
+          são "Home", de Edith Whiskers, e "She", de "Harry Styles" (músicas),
+          "Saiki Kusuo no Psi-nan" e "Shigatsu wa Kimi no Uso" (animações) e
+          bolo de chocolate (comida). Lipe também prefere doces no lugar de
+          salgados.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Aboutme;
